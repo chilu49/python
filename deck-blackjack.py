4c033b13b566a9c051b1dd12847c15c655b5802b
unknown <ravinder.unix@gmail.com>
2017-12-04 21:28:00 -0700

    deck-blackjack-python-code

---
+++ deck-blackjack.py
@@ -0,0 +1,15 @@
+#from random import shuffle
+
+#ranks = range(2,11) + ['JACK', 'QUEEN', 'KING', 'ACE']
+#print ranks
+#suits = ['S', 'H', 'D', 'C']
+#print suits
+
+#def get_deck():
+#    """Return new deck of cards"""
+#    return [[rank,suit] for rank in ranks for suit in suits]
+#deck = get_deck()
+#shuffle(deck)
+#print deck
+#print len(deck)
+
